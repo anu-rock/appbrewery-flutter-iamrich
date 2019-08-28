6164ff1f6b5a89ff31bc2f80da6d2f4a341f8855
Anurag Bhandari <anurag.bhd@gmail.com>
2019-08-28 15:48:52 +0530

    Replace dummy image (Serena) with the final one (Diamond)
    * Add 'images' dir to hold image assets
    * Add the final image (diamond.png) inside images dir
    * Update pubspec to add assets config
    * Replace dummy NetworkImage with our AssetImage in Scaffold body

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey[900],
-          title: Text('I Am Rich'),
-        ),
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://www.wtatennis.com/sites/default/files/styles/highlight_news_mobile_1_33_1_2x/public/field/image/gettyimages-1170400015.jpg'),
-          ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Text('I Am Rich'),
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
         ),
       ),
     ),
-  );
+  ));
 }
